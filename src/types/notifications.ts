4ba8aab9227f75de354b085d20c041e826ab5dee
exieneko <exieneko@proton.me>
2026-07-15 15:26:30 +0200

    fix: fix notification kind

--- src/types/notifications.ts
+++ src/types/notifications.ts
@@ -18,12 +18,37 @@ export interface Notification extends Type<'Notification'> {
     /** Array of users that are important to this notification */
     users: User[]
 }
-export const Notification: Model<Notification, Record<string, any>, { kind: NotificationKind }> = {
+export const Notification: Model<Notification, Record<string, any>, { kind: string }> = {
     async new(fmt, value, opts) {
+        const kind: NotificationKind = match(opts.kind, [
+            ['device_follow_tweet_notification_entry', NotificationKind.NewTweets],
+            ['generic_magic_rec_pyle_recommended', NotificationKind.RecommendedTweets],
+            
+            ['users_mentioned_you', NotificationKind.Mentioned],
+            ['users_followed_you', NotificationKind.NewFollowers],
+            ['users_liked_your_tweet', NotificationKind.TweetLiked],
+            ['users_retweeted_your_tweet', NotificationKind.TweetRetweeted],
+            ['users_liked_your_retweet', NotificationKind.RetweetLiked],
+            ['users_retweeted_your_tweet', NotificationKind.RetweetRetweeted],
+            ['users_added_you_to_lists', NotificationKind.AddedToList],
+            ['users_subscribed_to_your_list', NotificationKind.ListSubscribedTo],
+            ['generic_poll_voter_summary', NotificationKind.PollFinished],
+            
+            ['generic_birdwatch_needs_your_help', NotificationKind.BirdwatchNoteNeedsHelp],
+            ['generic_birdwatch_helpful_valid_rater', NotificationKind.BirdwatchNoteRatedHelpful],
+            ['generic_birdwatch_not_helpful_valid_rater', NotificationKind.BirdwatchNoteRatedNotHelpful],
+            ['generic_birdwatch_delete_post_rater', NotificationKind.BirdwatchNoteRatedDeleted],
+            
+            ['generic_login_notification', NotificationKind.LoggedIn],
+            ['generic_report_received', NotificationKind.ReportReceived],
+            ['generic_report_update', NotificationKind.ReportUpdate],
+            ['generic_subscription_promotion_premium', NotificationKind.Advertisement]
+        ], NotificationKind.Unknown);
+
         const _t = await fmt.next(TweetKind, value.tweet_results?.result);
         const tweet = _t.__typename === 'Tweet' ? _t : undefined;
 
-        const objectId = match(opts.kind, [
+        const objectId = match(kind, [
             [NotificationKind.Mentioned, tweet?.id],
             [[NotificationKind.TweetLiked, NotificationKind.TweetRetweeted, NotificationKind.RetweetLiked, NotificationKind.RetweetRetweeted], value.template?.target_objects?.at(0)?.result?.rest_id],
             [[NotificationKind.AddedToList, NotificationKind.ListSubscribedTo], value.notification_url?.url?.match(/list\/(\d+?)$/)?.at(1)],
@@ -46,12 +71,12 @@ export const Notification: Model<Notification, Record<string, any>, { kind: Noti
             id: value.id || tweet?.id,
             createdAt: new Date(value.timestamp_ms).toISOString(),
             objectId,
-            text: match(opts.kind, [
+            text: match(kind, [
                 [NotificationKind.AddedToList, NotificationKind.ListSubscribedTo, value.rich_message?.text?.match(/List\s(.*?)$/)?.at(1)],
                 [[NotificationKind.BirdwatchNoteNeedsHelp, NotificationKind.BirdwatchNoteRatedHelpful, NotificationKind.BirdwatchNoteRatedNotHelpful, NotificationKind.BirdwatchNoteRatedDeleted], value.template?.additional_context?.text],
                 [NotificationKind.ReportUpdate, value?.rich_message?.text]
             ]),
-            kind: opts.kind,
+            kind,
             tweets: tweets.filter(tweet => tweet?.__typename === 'Tweet'),
             users: users.filter(user => user?.__typename === 'User')
         };
